e519f2d39bae45140262c3bf26bb5aaf94ddcbe9
EneaSema <sema.enea1993@gmail.com>
2025-05-27 16:27:09 +0200

    start-working

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import { useState } from "react";
+import movies from "./assets/data/movies";
 
 function App() {
   return (
     <>
-      <h1>React Movie Filter </h1>
+      <div className="container">
+        <h1>React Movie Filter</h1>
+        <h2>Lista dei film da scegliere</h2>
+        {movies.map((movie, index) => (
+          <ul className="point-ulist">
+            <li key={index}>
+              {movie.title} {movie.genre}
+            </li>
+          </ul>
+        ))}
+      </div>
     </>
   );
 }
